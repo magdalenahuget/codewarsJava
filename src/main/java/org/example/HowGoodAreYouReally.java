4f7870e43e2ad54262cf68ebca7fc2df9440a6b4
magdalenahuget <magdalena.huget@gmail.com>
2023-05-06 14:40:18 +0200

    Add How Good Are You Really

---
+++ src/main/java/org/example/HowGoodAreYouReally.java
@@ -0,0 +1,27 @@
+package org.example;
+
+public class HowGoodAreYouReally {
+    public static void main(String[] args) {
+        int[] classPoints = {5,4,3,2,1};
+        int yourPoints = 4;
+        System.out.println(betterThenAverage(classPoints, yourPoints));
+        System.out.println();
+    }
+
+    public static boolean betterThenAverage(int[] classPoints, int yourPoints) {
+
+        int average = 0;
+
+        for(int onePersonPoints : classPoints){
+            average += onePersonPoints;
+        }
+
+        average = average / classPoints.length;
+        System.out.println(average);
+        if(average < yourPoints){
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
