fd52a03c323d678dabd9034ba01e3af5d18225f3
magdalenahuget <magdalena.huget@gmail.com>
2023-06-03 23:57:03 +0200

    Add calculate average

---
+++ src/main/java/org/example/CalculateAverage.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class CalculateAverage {
+    public static void main(String[] args) {
+
+        int[] testArray = {1,2,3,4,5};
+        System.out.println(find_average(testArray));
+    }
+
+    public static double find_average(int[] array){
+        double result = 0;
+        for(double element:array){
+            result+=element;
+        }
+        return result = result/array.length;
+    }
+}
