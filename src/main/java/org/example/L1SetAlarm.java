c1eacea16f926d6d40516e3ec38c555289df4e64
magdalenahuget <magdalena.huget@gmail.com>
2023-06-11 23:59:17 +0200

    Add return string

---
+++ src/main/java/org/example/L1SetAlarm.java
@@ -0,0 +1,14 @@
+//package org.example;
+//
+//public class L1SetAlarm {
+//    public static void main(String[] args) {
+//        System.out.println("Hello and welcome!");
+//        System.out.println();
+//    }
+//
+//
+//    public static boolean setAlarm(boolean employed, boolean vacation) {
+//        // Your code here...
+//    }
+//
+//}
