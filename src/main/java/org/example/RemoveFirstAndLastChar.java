641980b50640bc841f289719a68d75fb1164dce6
magdalenahuget <magdalena.huget@gmail.com>
2023-06-24 23:59:13 +0200

    Add Remove First and Last Character

---
+++ src/main/java/org/example/RemoveFirstAndLastChar.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class RemoveFirstAndLastChar {
+    public static void main(String[] args) {
+        System.out.println("Hello and welcome!");
+        System.out.println();
+    }
+
+}
