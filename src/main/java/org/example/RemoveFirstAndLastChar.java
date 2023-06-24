b7677ff010d30875bef17122f0bbdbca8028b543
magdalenahuget <magdalena.huget@gmail.com>
2023-06-25 00:05:14 +0200

    Add solution to remove first and last char

--- src/main/java/org/example/RemoveFirstAndLastChar.java
+++ src/main/java/org/example/RemoveFirstAndLastChar.java
@@ -2,8 +2,15 @@ package org.example;
 
 public class RemoveFirstAndLastChar {
     public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
-        System.out.println();
+        String str = "Summer time";
+        System.out.println(remove(str));
     }
 
+
+    public static String remove(String str) {
+        StringBuilder sb = new StringBuilder(str);
+        sb.deleteCharAt(sb.length() - 1);
+        sb.deleteCharAt(0);
+        return sb.toString();
+    }
 }
