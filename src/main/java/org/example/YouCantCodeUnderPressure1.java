cadba1fd5c364c4cc8adc34bba28f0522507a6fd
magdalenahuget <magdalena.huget@gmail.com>
2023-07-01 23:43:13 +0200

    Add resolution under presure

---
+++ src/main/java/org/example/YouCantCodeUnderPressure1.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class YouCantCodeUnderPressure1 {
+        public static int doubleInteger(int i) {
+            // Double the integer and return it!
+            return 2*i;
+        }
+}
