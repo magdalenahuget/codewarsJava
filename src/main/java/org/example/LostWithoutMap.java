9e4423147bd280c134ff0e59c60741108d99d2a7
magdalenahuget <magdalena.huget@gmail.com>
2023-07-12 23:41:24 +0200

    Add lost wihout map

---
+++ src/main/java/org/example/LostWithoutMap.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import java.util.Arrays;
+
+
+public class LostWithoutMap {
+    public static void main(String[] args) {
+        System.out.println("Hello and welcome!");
+        int[] arrays = {4, 5, 6};
+        int[] arraysNew = map(arrays);
+        System.out.println(Arrays.toString(arraysNew));
+    }
+
+    public static int[] map(int[] arr) {
+        int[] newArr = Arrays.stream(arr).map(el -> el * 2).toArray();
+        return newArr;
+
+    }
+}
